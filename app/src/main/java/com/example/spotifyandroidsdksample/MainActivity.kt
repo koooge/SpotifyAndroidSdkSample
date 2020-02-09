060528eddd869da3a209c3f405d6d0f35bbfa7eb
koooge <koooooge@gmail.com>
2020-02-10 00:35:09 +0100

    chore: Import spotify-app-remote-relesae-0.7.0
    Signed-off-by: koooge <koooooge@gmail.com>

--- app/src/main/java/com/example/spotifyandroidsdksample/MainActivity.kt
+++ app/src/main/java/com/example/spotifyandroidsdksample/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.spotifyandroidsdksample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // We will start writing our code here.
+    }
+
+    private fun connected() { // Then we will write some more code here.
+    }
+
+    override fun onStop() {
+        super.onStop()
+        // Aaand we will finish off here.
     }
 }
